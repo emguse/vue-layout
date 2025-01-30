06e5e2cb7a4f0a82668ddf48424fb26d1381d6cc
emguse <30695541+emguse@users.noreply.github.com>
2025-01-30 21:27:05 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import MyCanvas from './components/MyCanvas.vue'
+</script>
+
+<template>
+  <div>
+    <!-- 幅800, 高さ500のキャンバスを描画 -->
+    <MyCanvas :width="1100" :height="800" />
+  </div>
+</template>
+
+<style scoped>
+/* 必要に応じてスタイル調整 */
+</style>
